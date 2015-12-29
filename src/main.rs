96101e6b8321b2d6c7ccf5d30513eab1e6eea7bc
Matthew Nicholson <matt@matt-land.com>
2015-12-29 00:28:47 -0500

    initial version, missing many features
    It produces the correct count but does not support command line
    switches and does not format output like gnu wc yet. Also is not
    optimized.

---
+++ src/main.rs
@@ -0,0 +1,86 @@
+use std::env;
+use std::process;
+use std::path::Path;
+use std::io::BufReader;
+use std::io::prelude::*;
+use std::fs::File;
+use std::cmp::max;
+
+fn main() {
+    if env::args().count() != 2 {
+        println!("error: invalid arguments");
+        process::exit(1);
+    }
+
+    let path = env::args().nth(1).unwrap();
+    let path = Path::new(&path);
+
+    let file = match File::open(&path) {
+        Ok(f) => f,
+        Err(e) => {
+            println!("error opening file: {:?}", e);
+            process::exit(1);
+        }
+    };
+
+    let metadata = match file.metadata() {
+        Ok(m) => m,
+        Err(e) => {
+            println!("error reading file metadata: {:?}", e);
+            process::exit(1);
+        }
+    };
+
+    if metadata.is_dir() {
+        println!("{:?} is a directory", path);
+        process::exit(1);
+    }
+
+
+    enum State {
+        Whitespace,
+        Word,
+    }
+
+    let mut newline_count = 0;
+    let mut word_count = 0;
+    let mut byte_count = 0;
+    let mut char_count = 0;
+    let mut max_line_length = 0;
+    let mut current_line_length = 0;
+
+    let mut state = State::Whitespace;
+    let reader = BufReader::new(file);
+    for c in reader.bytes() {
+        byte_count += 1;
+        char_count += 1; // XXX what is a char?
+        let c = c.unwrap() as char;
+        match c {
+            '\n' => {
+                newline_count += 1;
+                max_line_length = max(max_line_length, current_line_length);
+                current_line_length = 0;
+            }
+            _ => current_line_length += 1,
+        }
+
+        state = match state {
+            State::Whitespace if !c.is_whitespace() => {
+                word_count += 1;
+                State::Word
+            }
+            State::Word if c.is_whitespace() => State::Whitespace,
+            state => state
+        }
+
+        // Print newline, word, and byte counts for each FILE, and a total line if
+        // more than one FILE is specified.  With no FILE, or when FILE is -, read
+        // standard  input.   A  word  is a non-zero-length sequence of characters
+        // delimited by white space.  The options below  may  be  used  to  select
+        // which counts are printed, always in the following order: newline, word,
+        // character, byte, maximum line length.
+    }
+
+    // XXX the formatting is such that each field takes up the space of the longest output field
+    println!("{} {} {} {} {} {:?}", newline_count, word_count, char_count, byte_count, max_line_length, path);
+}
