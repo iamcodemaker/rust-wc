6d481574cfcf0d24aef85f32aae710a599355db8
Matthew Nicholson <matt@matt-land.com>
2015-12-29 23:24:28 -0500

    move the counting functionality into a function
    Bounus: now unit testable!

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,54 @@
 use std::env;
 use std::process;
 use std::path::Path;
-use std::io::BufReader;
+use std::io::{self, BufReader};
 use std::io::prelude::*;
 use std::fs::File;
 use std::cmp::max;
 extern crate rust_wc;
 use rust_wc::counter::Count;
+
+/// Generate newline, word, character, byte, and maximum line length counts for the given iterator
+/// over a set of bytes. A word is a non-zero-length sequence of characters delimited by white
+/// space.
+fn count<I>(bytes: I) -> Count
+    where I: Iterator<Item=io::Result<u8>>
+{
+    enum State {
+        Whitespace,
+        Word,
+    }
+
+    let mut count = Count::new();
+    let mut current_line_length = 0;
+
+    let mut state = State::Whitespace;
+    for c in bytes {
+        count.bytes += 1;
+        count.chars += 1; // XXX what is a char?
+        let c = c.unwrap() as char;
+        match c {
+            '\n' => {
+                count.newlines += 1;
+                count.max_line_length = max(count.max_line_length, current_line_length);
+                current_line_length = 0;
+            }
+            _ => current_line_length += 1,
+        }
+
+        state = match state {
+            State::Whitespace if !c.is_whitespace() => {
+                count.words += 1;
+                State::Word
+            }
+            State::Word if c.is_whitespace() => State::Whitespace,
+            state => state
+        }
+
+    }
+
+    count
+}
 
 fn main() {
     if env::args().count() != 2 {
@@ -44,40 +86,9 @@ fn main() {
         Word,
     }
 
-    let mut count = Count::new();
-    let mut current_line_length = 0;
-
-    let mut state = State::Whitespace;
     let reader = BufReader::new(file);
-    for c in reader.bytes() {
-        count.bytes += 1;
-        count.chars += 1; // XXX what is a char?
-        let c = c.unwrap() as char;
-        match c {
-            '\n' => {
-                count.newlines += 1;
-                count.max_line_length = max(count.max_line_length, current_line_length);
-                current_line_length = 0;
-            }
-            _ => current_line_length += 1,
-        }
+    let count = count(reader.bytes());
 
-        state = match state {
-            State::Whitespace if !c.is_whitespace() => {
-                count.words += 1;
-                State::Word
-            }
-            State::Word if c.is_whitespace() => State::Whitespace,
-            state => state
-        }
-
-        // Print newline, word, and byte counts for each FILE, and a total line if
-        // more than one FILE is specified.  With no FILE, or when FILE is -, read
-        // standard  input.   A  word  is a non-zero-length sequence of characters
-        // delimited by white space.  The options below  may  be  used  to  select
-        // which counts are printed, always in the following order: newline, word,
-        // character, byte, maximum line length.
-    }
 
     // XXX the formatting is such that each field takes up the space of the longest output field
     println!("{} {} {} {} {} {:?}", count.newlines, count.words, count.chars, count.bytes, count.max_line_length, path);
