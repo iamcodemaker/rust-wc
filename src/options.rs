4e06e1c778b1b2f93fc4687fa501cfb51d52bfc9
Matthew Nicholson <matt@matt-land.com>
2016-01-02 18:58:22 -0500

    handle -cmlLw options to fliter counts
    Also add tests and handle defaults when no options are passed.

--- src/options.rs
+++ src/options.rs
@@ -9,6 +9,11 @@ pub type Result = result::Result<Options, Box<Error>>;
 
 pub struct Options {
     matches: getopts::Matches,
+    pub bytes: bool,
+    pub chars: bool,
+    pub lines: bool,
+    pub max_line: bool,
+    pub words: bool,
 }
 
 impl Options {
@@ -20,16 +25,73 @@ impl Options {
         where I: Iterator,
         I::Item: AsRef<OsStr>,
     {
+        let mut opts = getopts::Options::new();
+        opts.optflag("c", "bytes", "print the byte counts");
+        opts.optflag("m", "chars", "print the character counts");
+        opts.optflag("l", "lines", "print the newline counts");
+        opts.optflag("L", "max-line-length", "print the length of the longest line");
+        opts.optflag("w", "words", "print the word counts");
 
         // we do skip(1) here because the first argument is the program name
         let matches = try!(opts.parse(args.skip(1)));
 
-        Ok(Options {
+        let mut opts = Options {
+            bytes: matches.opt_present("c"),
+            chars: matches.opt_present("m"),
+            lines: matches.opt_present("l"),
+            max_line: matches.opt_present("L"),
+            words: matches.opt_present("w"),
             matches: matches,
-        })
+        };
+
+        // if no options are provided, set some defaults
+        if !(opts.bytes || opts.chars || opts.lines || opts.max_line || opts.words) {
+            opts.lines = true;
+            opts.words = true;
+            opts.bytes = true;
+        }
+
+        Ok(opts)
     }
 
     pub fn files(&self) -> Iter<String> {
         self.matches.free.iter()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Options;
+
+    #[test]
+    fn defaults() {
+        let args = vec!["test", "file"];
+        let opts = Options::from_iter(args.iter()).unwrap();
+        assert!(opts.lines);
+        assert!(opts.words);
+        assert!(opts.bytes);
+    }
+
+    #[test]
+    fn args_bytes() {
+        {
+            let args = vec!["test", "-c"];
+            let opts = Options::from_iter(args.iter()).unwrap();
+            assert!(opts.bytes);
+            assert!(!opts.lines);
+            assert!(!opts.words);
+            assert!(!opts.chars);
+            assert!(!opts.max_line);
+        }
+
+        {
+            let args = vec!["test", "--bytes"];
+            let opts = Options::from_iter(args.iter()).unwrap();
+            assert!(opts.bytes);
+            assert!(!opts.lines);
+            assert!(!opts.words);
+            assert!(!opts.chars);
+            assert!(!opts.max_line);
+        }
     }
 }
