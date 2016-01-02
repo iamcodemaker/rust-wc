78a6c69c4e8e697c0af477cbf5bca586c05ed377
Matthew Nicholson <matt@matt-land.com>
2016-01-02 18:57:24 -0500

    make Options unit testable

--- src/options.rs
+++ src/options.rs
@@ -3,6 +3,7 @@ use std::env;
 use std::slice::Iter;
 use std::error::Error;
 use std::result;
+use std::ffi::OsStr;
 
 pub type Result = result::Result<Options, Box<Error>>;
 
@@ -12,10 +13,16 @@ pub struct Options {
 
 impl Options {
     pub fn new() -> Result {
-        let opts = getopts::Options::new();
+        Self::from_iter(env::args_os())
+    }
+
+    fn from_iter<I>(args: I) -> Result
+        where I: Iterator,
+        I::Item: AsRef<OsStr>,
+    {
 
         // we do skip(1) here because the first argument is the program name
-        let matches = try!(opts.parse(env::args().skip(1)));
+        let matches = try!(opts.parse(args.skip(1)));
 
         Ok(Options {
             matches: matches,
